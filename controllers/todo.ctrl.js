f01544279a325763e8b27b5a6071f59b11c19937
Igor Znamensky <igor.znamensky@gmail.com>
2022-07-22 13:37:47 +0300

    Bootstrap UI

--- controllers/todo.ctrl.js
+++ controllers/todo.ctrl.js
@@ -1,11 +1,17 @@
 const ToDo = require('../models/toDo.mongo');
 
+const checkBody = (body) => {
+  if(body.text.length || body.body.length) {
+      return true
+  }
+  return false
+}
+
 // @desc        Add toDo
 // @route       POST /todo/add
 // @access      Private/Admin
 exports.addToDo = async (req, res, next) => {
-    const { text, body } = req.body
-    if(text.length === 0 || body.length === 0) return res.redirect('/')
+    if(!checkBody(req.body)) return res.redirect('/')
     await ToDo.create(req.body, async (err) => {
         if (err) return console.log(err)
         res.redirect('/')
@@ -21,5 +27,29 @@ exports.deleteToDo = async (req, res, next) => {
     await ToDo.findByIdAndDelete(id)
 
     res.redirect(204, '/')
+
+};
+
+// @desc        EDIT Todo
+// @route       POST /todo/edit/:id
+// @access      Private/Admin
+exports.editToDo = async (req, res, next) => {
+    const id = req.params.id
+    if (checkBody(req.body)){
+        await ToDo.findByIdAndUpdate(id, req.body)
+        res.redirect(204, '/')
+    }
+
+};
+
+// @desc        GET Single Todo
+// @route       GET /todo/get/:id
+// @access      Private/Admin
+exports.getSingleToDo = async (req, res, next) => {
+    const id = await req.params.id
+    const toDos = await ToDo.findById(id);
+
+    res
+        .render('todoEdit', { todo: toDos})
 
 };
--- routes/toDo.routes.js
+++ routes/toDo.routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { addToDo, deleteToDo} = require("../controllers/todo.ctrl");
+const { addToDo, deleteToDo, getSingleToDo} = require("../controllers/todo.ctrl");
 const router = express.Router();
 
 router
+    .get('/get/:id', getSingleToDo)
     .delete('/delete/:id', deleteToDo)
+    .post('/edit/:id', addToDo)
     .post('/add', addToDo);
 
 
